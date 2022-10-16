568a9965226cc0c8cce346a633407960d745f056
Jennifer Herr <jenh11@gmail.com>
2022-10-16 00:07:34 -0700

    Initial Commit

---
+++ readme.txt
@@ -0,0 +1,7 @@
+This is one of many projects I've worked on in my quest to become a web developer.  It is from the Udemy course by Jonas Schmedtmann titled "The Complete JavaScript Course: From Zero to Expert".  This was a tutorial style project.  I've made minimal changes to the style, and refactored the JavaScript code.
+
+Like the Drum Kit project, this project introduced Event Listeners and using JavaScript to modify page contents.  I liked this project a little more though because I was able to practice multiple related concepts beyond Event Listeners, such as developing game logic, and differing button functionality.  
+
+I feel like I'm getting a much better handle on DOM syntax and JavaScript function building.  
+
+I've uploaded it here to continue documenting my journey.  When I am better with CSS, I hope to revisit this, and other similar tutorial projects, to enhance functionality and develop my own design.  
---
+++ script.js
@@ -0,0 +1,67 @@
+'use strict';
+
+let score = 20;
+let highScore = 0;
+
+const generateNumber = function () {
+  let secretNumber = Math.round(Math.random() * 100);
+  console.log(secretNumber);
+  return secretNumber;
+}
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+}
+const changeBackground = function (color) {
+  document.querySelector('body').style.backgroundColor = color;
+}
+const revealNumber = function (number) {
+  document.querySelector('.number').textContent = number;
+}
+const updateScore = function (score) {
+  document.querySelector('.score').textContent = score;
+}
+let secretNumber = generateNumber();
+  
+// Event Listener for Game
+document.querySelector('.check').addEventListener('click', function () {
+  let guess = Number(document.querySelector('.guess').value);
+
+  // For empty guess box
+  if (!guess) {
+    displayMessage("<- Input a Number!");
+  }
+    
+  // For winning guess
+  else if (guess === secretNumber) {
+    displayMessage("Correct!");
+    changeBackground("#104911");
+    revealNumber(secretNumber);
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
+  }
+    
+  // For non-winning guess
+  else if (guess !== secretNumber) {
+    displayMessage(guess > secretNumber ? "Too High!" : "Too Low!")
+    score--;
+    updateScore(score);
+    }
+
+  // for Game Over
+  else if (score < 1) {
+    displayMessage("Game Over!");
+    changeBackground("#BD4C52");
+  }
+})
+
+// Reset Button
+document.querySelector(".again").addEventListener("click", function () {
+  secretNumber = generateNumber();
+  updateScore(20);
+  displayMessage("Start Guessing...");
+  changeBackground("#222");
+  revealNumber("?");
+  document.querySelector('.guess').value = " ";
+  })
